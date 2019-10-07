c9657c603ac1f28275e3fe859fb6d219b7acd5f0
MDDeberry <MDDeberry@gmail.com>
2019-10-07 18:21:11 -0500

    Transfer of Files from Other Repository
    This includes the name and favico update

---
+++ public/javascripts/menuBar.js
@@ -0,0 +1,148 @@
+// FILE: menuBar.js
+// AUTHORS: Justin Erickson, Richie Burch, Matt Hardin, Nathan Robertson
+// PURPOSE: Controls behavior of the toolbar. Most functions in this file are triggered upon an event happening.
+
+const PREAMBLE = "from mine import *\n\n" +
+    "mc = Minecraft()\n" +
+    "\n";
+
+const NOTIFY_OPTIONS = {autoHideDelay: 10000};
+
+
+// Let pressing enter inside fileName textbox trigger save file event
+// https://stackoverflow.com/questions/155188/trigger-a-button-click-with-javascript-on-the-enter-key-in-a-text-box
+$("#fileNameTextBox").keyup(function (event) {
+   if (event.keyCode === 13) {
+       $("#generateButton").click();
+   }
+});
+
+
+/**
+ * createSnapshot: Allows user to save current workspace.
+ */
+// https://groups.google.com/forum/#!topic/blockly/NDlC-l6DLEM
+function createSnapshot() {
+    let xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    let xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+    localStorage.setItem("blockly.xml", xmlText);
+    displaySuccessNotification(".menu","Snapshot created");
+}
+
+/**
+ * restoreSnapshot: Restores a saved workspace in browser.
+ */
+function restoreSnapshot() {
+    let xmlText = localStorage.getItem("blockly.xml");
+    if (xmlText) {
+        Blockly.mainWorkspace.clear();
+        let xmlDom = Blockly.Xml.textToDom(xmlText);
+        Blockly.Xml.domToWorkspace(xmlDom, Blockly.mainWorkspace);
+    }
+    displaySuccessNotification(".menu", "Snapshot restored");
+}
+
+
+/**
+ * loadBlocks: Reads an XML file and loads it into workspace.
+ */
+function loadBlocks() {
+    let selectedFile = document.getElementById('file-input').files[0];
+    let fileReader = new FileReader();
+    if (selectedFile.type === "text/xml") {
+        fileReader.readAsText(selectedFile);
+        fileReader.onload = function() {
+            let data = fileReader.result;
+            Blockly.mainWorkspace.clear();
+            let xmlDom = Blockly.Xml.textToDom(data);
+            Blockly.Xml.domToWorkspace(xmlDom, Blockly.mainWorkspace);
+        };
+    }
+    displaySuccessNotification(".menu","Load was successful.");
+}
+
+
+/**
+ * saveBlocks: Download current workspace to user's pc as an XML file.
+ */
+function saveBlocks() {
+    let xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    let xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+    let xmlBlob = new Blob([xmlText], {type: "text/plain"});
+    let textToSaveAsURL = window.URL.createObjectURL(xmlBlob);
+    createDownloadLink(textToSaveAsURL).click();
+}
+
+function createDownloadLink(textToSaveAsURL) {
+    let downloadLink = document.createElement("a");
+    downloadLink.download = 'export.xml';
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    return downloadLink;
+}
+
+// Helper for export blocks.
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
+
+/**
+ * createScript: Sends request to server to record code in Pycraft directory
+ */
+function createScript() {
+    if (workspaceHasWarnings()) {
+        $('.menu').notify("Must resolve warnings before creating script.", "error", NOTIFY_OPTIONS);
+    }
+    else {
+        sendCode();
+    }
+}
+
+/**
+ * sendCode: Sends generated Blockly code via AJAX to the server.
+ */
+function sendCode() {
+    let codeForm = new FormData();
+    let xhttp = new XMLHttpRequest();
+    Blockly.Python.INFINITE_LOOP_TRAP = null;
+    let code = PREAMBLE + Blockly.Python.workspaceToCode(mainWorkspace);
+    codeForm.append("codeArea", code);
+    codeForm.append("fileName", document.getElementById("fileNameTextBox").value);
+    xhttp.open("POST", "/copy_text", true);
+    addLoadEvent(xhttp);
+    xhttp.send(codeForm);
+}
+
+/**
+ * A predicate helper function for createScript.
+ * Returns true if any blocks in workspace have a warning assigned to them or false otherwise.
+ * Note: !! is necessary as it casts a variable into a boolean expression and then returns
+ * true if variable is not undefined or false otherwise.
+ */
+function workspaceHasWarnings() {
+    let blocks = mainWorkspace.getAllBlocks();
+    return !!blocks.find((block) => !!block.warning);
+}
+
+/**
+ * Displays notification to user based on result of AJAX query.
+ * @param xhttp: Object representing the AJAX transaction.
+ */
+function addLoadEvent(xhttp) {
+    xhttp.addEventListener('load', function () {
+        let response = JSON.parse(xhttp.responseText);
+        if (response['errors'] === undefined) {
+            displaySuccessNotification(".menu", "File: " + response.file_name + " saved");
+        } else {
+            $(".menu").notify(response.errors[0].msg, "error", NOTIFY_OPTIONS);
+        }
+    });
+}
+
+// Source: https://notifyjs.jpillora.com/
+function displaySuccessNotification(element, notificationText) {
+    $(element).notify(notificationText, "success", NOTIFY_OPTIONS);
+}
---
+++ public/javascripts/minecraft_blocks/player.js
@@ -0,0 +1,157 @@
+// FILE: player.js
+// AUTHOR: Richie Burch; Nathan Robertson
+// PURPOSE: Implement google_blocks for all methods in the Minecraft.Player class
+
+
+// Each block is implemented in two parts: First the new block is added to the Block associative array.
+// Then the appropriate python conversion function is added to the Blockly.Python associative array.
+
+
+const BLOCK_COLOR = 230;
+
+
+Blockly.defineBlocksWithJsonArray([
+    {
+        // BLOCK: mc.player.getPos()
+        "type": "player_get_position",
+        "message0": "Get players position.",
+        "output": "Vector",
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "tooltip": "Gets players position in the world. Returns coordinates as vector.",
+        "helpUrl": ""
+    },
+    {
+        // BLOCK: mc.player.setPos()
+        "type": "player_set_position",
+        "message0": "Set players position to : %1",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "VEC3",
+            "check": "Vector"
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "extensions": ["empty_input_warning"],
+        "tooltip": "Moves player to provided position.",
+        "helpUrl": ""
+    },
+    {
+        // BLOCK: mc.player.getTilePos()
+        "type": "player_get_tile_position",
+        "message0": "Get position under player.",
+        "output": "Vector",
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "tooltip": "",
+        "helpUrl": ""
+    },  
+    {
+        // BLOCK: mc.player.setTilePos()
+        "type": "player_set_tile_position",
+        "message0": "Set players position on top of: %1",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "VEC3",
+            "check": "Vector"
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "extensions": ["empty_input_warning"],
+        "tooltip": "Move player on top of tile at provided position.",
+        "helpUrl": ""
+    },
+    {
+        // BLOCK:  mc.player.getDirection()
+        "type": "player_get_direction",
+        "message0": "Get players direction.",
+        "output": "Vector",
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "tooltip": "Get players direction. Returns a vector object.",
+        "helpUrl": ""
+    },
+    {
+        // BLOCK: mc.player.getRotation()
+        "type": "player_get_rotation",
+        "message0": "Get players angle of rotation.",
+        "output": "Number",
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "tooltip": "Get the angle of players rotation.",
+        "helpUrl": ""
+    },
+    // BLOCK: mc.player.getPitch()
+    {
+        "type": "player_get_pitch",
+        "message0": "Get players pitch.",
+        "output": "Number",
+        "colour": '%{BKY_MINECRAFT_HUE}',
+        "tooltip": "Gets the players pitch",
+        "helpUrl": ""
+    }
+]);
+
+// code generators
+Blockly.Python['player_get_position'] = function (block) {
+    let code = makeFunctionCall("mc.player.getPos");
+    if(block.getParent() === null) {
+        return [code+'\n', Blockly.Python.ORDER_NONE];
+    } else {
+        return [code, Blockly.Python.ORDER_NONE];
+    }
+};
+Blockly.Python['player_set_position'] = function (block) {
+    let vector3 = Blockly.Python.valueToCode(block,"VEC3", Blockly.Python.ORDER_ATOMIC);
+    return makeFunctionCall("mc.player.setPos", [vector3])+'\n';
+};
+Blockly.Python['player_get_tile_position'] = function (block) {
+    let code = makeFunctionCall("mc.player.getTilePos");
+    if(block.getParent() === null) {
+        return [code+'\n', Blockly.Python.ORDER_NONE];
+      } else {
+        return [code, Blockly.Python.ORDER_NONE];
+      }
+};
+Blockly.Python['player_set_tile_position'] = function (block) {
+    let vector3 = Blockly.Python.valueToCode(block,"VEC3", Blockly.Python.ORDER_ATOMIC);
+    let code = "mc.player.setTilePos("+vector3+")\n";
+    return code;
+};
+Blockly.Python['player_get_direction'] = function (block) {
+    let code = makeFunctionCall("mc.player.getDirection");
+    if(block.getParent() === null) {
+        return [code+'\n', Blockly.Python.ORDER_NONE];
+      } else {
+        return [code, Blockly.Python.ORDER_NONE];
+      }
+};
+Blockly.Python['player_get_rotation'] = function (block) {
+    let code = makeFunctionCall("mc.player.getRotation");
+    if(block.getParent() === null) {
+        return [code+'\n', Blockly.Python.ORDER_NONE];
+      } else {
+        return [code, Blockly.Python.ORDER_NONE];
+      }
+};
+Blockly.Python['player_get_pitch'] = function (block) {
+    let code = makeFunctionCall("mc.player.getPitch");
+    if(block.getParent() === null) {
+        return [code+'\n', Blockly.Python.ORDER_NONE];
+      } else {
+        return [code, Blockly.Python.ORDER_NONE];
+      }
+};
+
+// HELPERS
+
+/**
+ * @function makeFunctionCall: Constructs a string representing a python function call
+ * @param functionName: string
+ * @param arguments: array (empty by default)
+ */
+let makeFunctionCall = (functionName, arguments=[]) => {
+    return functionName + "(" + arguments.join(", ") + ")";
+};
